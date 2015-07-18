3d6daab3bf956794a2893cca9058eb2dc5a5889f
Tomas Herma <tomas.herma@letsgood.com>
2015-07-18 17:00:26 +0200

    bug fixed with sockets connection

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
 var Synergykit = require("./lib/synergykit").Synergykit
 module.exports = Synergykit
-
-
-Synergykit.Init("synergykit-sample-app", "83c04c28-4b6b-4f86-a4f8-ab357ead768c", {
-    debug: true,
-    local: true,
-    strategy: "sockets"
-})
-var gameScore = Synergykit.Data("GameScore")
-gameScore.on("created", function(data) {
-    console.log(data.get("score"))
-})
